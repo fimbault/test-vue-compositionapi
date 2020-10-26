90787c364009dc1cdbacbd3ea2c0f737ba041cc2
Fabien <45664395+fimbault@users.noreply.github.com>
2020-10-26 13:11:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld msg="composition API test"/>
+  <Counter/>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Counter
+  }
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>{{ count }}</div>
+  <button @click="increment">+1</button>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue'
+
+// properly infer types in typescript
+export default defineComponent({
+  name: 'Counter',
+  setup() {
+    const count = ref(0);
+
+    function increment() {
+      count.value++;
+    }
+
+    return {
+      count,
+      increment     
+    }
+  }
+});
+</script>
